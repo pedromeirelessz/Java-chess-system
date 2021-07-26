ab1e63c4277ee8e7732c7b8fd9b722af539a998b
phmeyreles <principalphmeyreles3@gmail.com>
2021-07-25 22:55:42 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -42,4 +42,8 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 }
